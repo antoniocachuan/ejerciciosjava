f97ddce9123e96895e1777076231124d14b29e6f
Antonio Cachuan <alipazaga07@gmail.com>
2013-07-13 20:22:59 -0500

    paquete dto

---
+++ TrabajadorApp/src/pe/edutec/trabajadorapp/dto/TrabajadorDto.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.edutec.trabajadorapp.dto;
+
+/**
+ *
+ * @author Alumno-CT
+ */
+public class TrabajadorDto {
+    
+}
